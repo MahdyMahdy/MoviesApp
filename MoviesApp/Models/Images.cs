598325781c9828412f2a252e25ea51de1b213180
Mehdi Mehdi <mehdi.mehdi@net.usj.edu.lb>
2022-02-07 01:03:42 +0200

    finish

---
+++ MoviesApp/Models/Images.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MoviesApp.Models
+{
+    public class Backdrop
+    {
+        [Key]
+        public int id { get; set; }
+        public double aspect_ratio { get; set; }
+        public int height { get; set; }
+        public string iso_639_1 { get; set; }
+        public string file_path { get; set; }
+        public double vote_average { get; set; }
+        public int vote_count { get; set; }
+        public int width { get; set; }
+        [ForeignKey("Images")]
+        public int Imagesid { get; set; }
+    }
+
+    public class Logo
+    {
+        [Key]
+        public int id { get; set; }
+        public double aspect_ratio { get; set; }
+        public int height { get; set; }
+        public string iso_639_1 { get; set; }
+        public string file_path { get; set; }
+        public double vote_average { get; set; }
+        public int vote_count { get; set; }
+        public int width { get; set; }
+        [ForeignKey("Images")]
+        public int Imagesid { get; set; }
+    }
+
+    public class Poster
+    {
+        [Key]
+        public int id { get; set; }
+        public double aspect_ratio { get; set; }
+        public int height { get; set; }
+        public string iso_639_1 { get; set; }
+        public string file_path { get; set; }
+        public double vote_average { get; set; }
+        public int vote_count { get; set; }
+        public int width { get; set; }
+        [ForeignKey("Images")]
+        public int Imagesid { get; set; }
+    }
+
+    public class Images
+    {
+        public List<Backdrop> backdrops { get; set; }
+        [Key]
+        public int id { get; set; }
+        public List<Logo> logos { get; set; }
+        public List<Poster> posters { get; set; }
+    }
+}
---
+++ MoviesApp/Models/MovieDetails.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MoviesApp.Models
+{
+    public class MovieDetails
+    {
+        public MovieDetails(Movie movie,Images images)
+        {
+            this.movie = movie;
+            this.images = images;
+        }
+        public Movie movie { get; set; }
+        public Images images { get; set; }
+    }
+}
